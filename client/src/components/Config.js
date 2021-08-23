c1f2d4b17ff52baf59f8b0133323134ecd3854d3
LAPTOP-9KDDCU7B\kkkuk <kkuk090@gmail.com>
2021-08-23 23:29:16 +0900

    react-movie-app start

---
+++ client/src/components/Config.js
@@ -0,0 +1,6 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL= 'https://api.themoviedb.org/3/';
+export const API_KEY = '5a3a842980c7ec2b48b0e2cf66321744';
+export const IMAGE_BASE_URL='http://image.tmdb.org/t/p/';
