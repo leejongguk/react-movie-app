33e012a031fff9d386c879f12af98aa0d6bccb8d
LAPTOP-9KDDCU7B\kkkuk <kkuk090@gmail.com>
2021-08-24 23:37:52 +0900

    MovieList-LoadMore,MovieDetail

--- client/src/components/views/LandingPage/Section/MainImage.js
+++ client/src/components/views/LandingPage/Section/MainImage.js
@@ -8,8 +8,8 @@ function MainImage(props){
         100%),
         url('${props.image}'), #1c1c1c`,
             height: '500px',
-            backgroundSize: '500px, cover',
-            backgroundPosition: 'center,center',
+            backgroundSize: '100%, cover',
+            backgroundPosition: 'center, center',
             width: '100%',
             position: 'relative'        
         }}>
